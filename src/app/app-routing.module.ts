f97229f425ec456c8cce3997d70e8d76e07e4b12
Jefry Jhonatan Londoño Acosta - Ceiba Software <jefry.londono@ceiba.com.co>
2019-02-11 09:51:10 -0500

    componente parking formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
-import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/appComponent', pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParkingComponent } from './parking/parking.component';
+// import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,11 @@ import { ParkingComponent } from './parking/parking.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
